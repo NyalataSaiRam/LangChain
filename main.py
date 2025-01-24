15eaab3ca259bdf3406a3325bfd388b69b57c51a
N Sai Ram <nyalatasairam@gmail.com>
2025-01-24 20:25:21 +0530

    add a conditional node

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from states import State
-from nodes import nodeOne
+from nodes import nodeOne, nodeTwo, nodeThree, conditionalNode
 
 
 # graph Definition
@@ -8,16 +8,30 @@ graph_builder = StateGraph(state_schema=State)
 
 # adding nodes
 graph_builder.add_node('nodeOne', nodeOne)
+graph_builder.add_node('nodeTwo', nodeTwo)
+graph_builder.add_node('nodeThree', nodeThree)
+graph_builder.add_node('conditionalNode', conditionalNode)
+
+
+# flow
+# START --> nodeONe --> conditionalNode --> either to nodeTwo or nodeThree --> END
+
 
 #defining edges
+# add_edge(arg1 : str, arg2 : str)
 graph_builder.add_edge(START, 'nodeOne')
-graph_builder.add_edge('nodeOne', END)
+
+# add_conditional_edges(arg1 : str, arg2 : callable -> str)
+graph_builder.add_conditional_edges('nodeOne', conditionalNode)
+graph_builder.add_edge('nodeTwo', END)
+graph_builder.add_edge('nodeThree', END)
 
 # compile graph
 graph = graph_builder.compile()
 
 
 # invoking the graph
+# invoke(arg : State)
 res = graph.invoke({"prompt": "define llm"})
 print(res['output'])
 
--- nodes.py
+++ nodes.py
@@ -1,7 +1,33 @@
 from states import State
-from genai import llm
+# from genai import llm
+from random import randint
 
 def nodeOne(state:State):
+
+    # res = llm.invoke(state["prompt"])
+    # return {"output": res.content}
+
+    return {"output": "From node one"}
+
+
+def nodeTwo(state:State):
+
+    return {"output": "From node two"}
+
+def nodeThree(state:State):
+
+    return {"output": "From node three"}
+
+def conditionalNode(state:State):
+
+    # generate random integer between 1 and 2
+    intVal : int = randint(1,2)
+
+    if intVal == 1:
+        return "nodeTwo"
     
-    res = llm.invoke(state["prompt"])
-    return {"output": res.content}
+    return "nodeThree"
+
+
+
+
