a3b8272836b75afd23f09ae7fd3c55845303285a
N Sai Ram <nyalatasairam@gmail.com>
2025-01-26 19:58:29 +0530

    summarize previous responses

---
+++ 5_summarization_of_previous_messages/main.py
@@ -0,0 +1,37 @@
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import StateGraph, START, END
+from classes import State
+from nodes import call_model, summarize_conversation, should_summarize
+from langchain_core.messages import HumanMessage
+
+
+
+# initialize memory
+memory: MemorySaver = MemorySaver()
+
+# configure conversation id's
+config = {"configurable": {"thread_id": "1"}}
+
+
+workFlow = StateGraph(state_schema = State)
+
+# nodes
+workFlow.add_node('conversation',  call_model)
+workFlow.add_node(summarize_conversation)
+
+# flow
+workFlow.add_edge(START, 'conversation')
+workFlow.add_conditional_edges('conversation', should_summarize)
+workFlow.add_edge('summarize_conversation', END)
+
+graph = workFlow.compile(checkpointer = memory)
+
+while True :
+    userInput = input('Enter prompt : ')
+    if userInput == 'q' :
+        break
+
+    res = graph.invoke({"messages" : [HumanMessage( content = userInput )]}, config = config)
+
+    print(res['messages'][-1].content)
+    print('\n')
