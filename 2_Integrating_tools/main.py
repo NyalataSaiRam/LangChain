02e5a51327e4cf1a9b40e662929587d6fbe9c35c
N Sai Ram <nyalatasairam@gmail.com>
2025-01-26 14:40:17 +0530

    add SystemMessage and finetune the output

--- 2_Integrating_tools/main.py
+++ 2_Integrating_tools/main.py
@@ -1,12 +1,13 @@
 
 from langgraph.graph import StateGraph, START, END, MessagesState
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 from nodes import call_func
 from genai import tools
 
 # this will execute the function tools decided by the llm
 from langgraph.prebuilt import ToolNode, tools_condition
 
+sysMsg = SystemMessage(content="you are math expert. you are AI Math Assistant");
 
 # MessagesState is provided by langgraph, it is similar to our defined class State
 graph_builder:StateGraph = StateGraph(state_schema=MessagesState)
@@ -22,14 +23,15 @@ graph_builder.add_edge(START, 'call_func')
 graph_builder.add_conditional_edges('call_func', tools_condition)
 
 # if the llm is using our tools then it will end here
-graph_builder.add_edge('tools', END)
+graph_builder.add_edge('tools', 'call_func')
 
 graph = graph_builder.compile()
 
-result = graph.invoke({"messages": HumanMessage(content="2+3")})
 
-for m in result["messages"]:
-    print(m.content)
+# added SystemMessage
+result = graph.invoke({"messages": [sysMsg, HumanMessage(content="10 times 20")]})
+
+print(result["messages"][-1].content)
 
 
 
