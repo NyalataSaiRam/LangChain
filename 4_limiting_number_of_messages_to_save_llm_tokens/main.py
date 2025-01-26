591b0ddff0f8581da3949a8a10dd9dba542a275b
N Sai Ram <nyalatasairam@gmail.com>
2025-01-26 18:14:42 +0530

    using inbuilt method

--- 4_limiting_number_of_messages_to_save_llm_tokens/main.py
+++ 4_limiting_number_of_messages_to_save_llm_tokens/main.py
@@ -1,7 +1,7 @@
 from langgraph.graph import StateGraph, START, END, MessagesState
 from langchain_core.messages import HumanMessage, SystemMessage
 from langgraph.checkpoint.memory import MemorySaver
-from nodes import call_llm, removeMessages
+from nodes import call_llm
 from genai import tools
 
 # this will execute the function tools decided by the llm
@@ -21,13 +21,11 @@ graph_builder:StateGraph = StateGraph(state_schema=MessagesState)
 
 # call_func renamed to call_llm
 graph_builder.add_node('call_llm',call_llm)
-graph_builder.add_node('removeMessages',removeMessages)
 
 graph_builder.add_node("tools", ToolNode(tools=tools))
 
 
-graph_builder.add_edge(START, 'removeMessages')
-graph_builder.add_edge('removeMessages', 'call_llm')
+graph_builder.add_edge(START, 'call_llm')
 
 # if llm uses its tool then it generate response and Ends there it self.
 graph_builder.add_conditional_edges('call_llm', tools_condition)
--- 4_limiting_number_of_messages_to_save_llm_tokens/nodes.py
+++ 4_limiting_number_of_messages_to_save_llm_tokens/nodes.py
@@ -1,12 +1,16 @@
 from langgraph.graph import MessagesState
-from langchain_core.messages import RemoveMessage
-from genai import llm_call_tools
+from langchain_core.messages import trim_messages
+from genai import llm_call_tools, llm
 
 # invokes the llm with userInput
 def call_llm(state: MessagesState):
-    return {"messages": llm_call_tools.invoke(state["messages"])}
-
-# removes the messages from list except the last four
-def removeMessages(state:MessagesState):
-    filteredMsgs = [RemoveMessage(id = m.id) for m in state["messages"][:-4]]
-    return {"messages":filteredMsgs}
+    trimmed_msgs = trim_messages(
+        state["messages"],
+        strategy = "last",
+        token_counter =llm,
+        max_tokens = 100,
+        start_on = 'human',
+        end_on = ('human', 'tool'),
+        include_system = False
+    )
+    return {"messages": llm_call_tools.invoke(trimmed_msgs)}
